d10060b989dda9267bca2e5c541676b31beda36b
heee1 <p1xell@kakao.com>
2023-06-12 13:37:08 +0900

    230612_1337

--- basic-types/never.ts
+++ basic-types/never.ts
@@ -26,5 +26,18 @@ if (typeof a !== 'string'){ // string, number에서 string을 제외한 상태
 // 위처럼 특정타입이 되도록 의도할 수 있다.
 
 
+// never가 활용되는 방식
 type Indexable<T> = T extends string ? T & { [index : string]: any } : never;
-// never 0546부터
+type ObjectIndexable = Indexable<{}>
+
+// 31번라인 {}가 30번 라인 <T>에 들어가는 것
+// 타입이 변수처럼 쓰이는 제네릭
+
+// 위는 "조건부 타입"으로 아직배우지 않았음
+// type alias의 indexable이라고하는 제레닉타입
+// t가 만약에 string이면, T라고하고 indexable타입을 추가하는 형태
+// 여기에 할당되는 값은 any이며 나머지인경우는 never
+
+
+// 제네릭 Generic 이란?
+// 타입을 마치 함수의 파라미털처럼 사용하는 것
