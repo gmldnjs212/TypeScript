d79f346e9939a2f73fd0a4dafda593a2fa4f586b
heee1 <p1xell@kakao.com>
2023-06-12 22:56:13 +0900

    230612_2256

--- basic-types/never.ts
+++ basic-types/never.ts
@@ -29,15 +29,22 @@ if (typeof a !== 'string'){ // string, number에서 string을 제외한 상태
 // never가 활용되는 방식
 type Indexable<T> = T extends string ? T & { [index : string]: any } : never;
 type ObjectIndexable = Indexable<{}>
+// const b: Indexable<{}> = ''; // 잘못된 타입의 값으 넣으려고 하면 에러발생
 
 // 31번라인 {}가 30번 라인 <T>에 들어가는 것
 // 타입이 변수처럼 쓰이는 제네릭
+
+// 코드 설명
+// T extends string ? => T가 만약 string이면 
+// T & { [index : string]: any } => 이렇게 만들어서 보내겠다
+// : never; => 그게 아니라면 never라고 하겠다.
+// 즉 {}에 잘못된 타입을 넣게되면 never가 나오는 것
+// 32번 라인 처럼
 
 // 위는 "조건부 타입"으로 아직배우지 않았음
 // type alias의 indexable이라고하는 제레닉타입
 // t가 만약에 string이면, T라고하고 indexable타입을 추가하는 형태
 // 여기에 할당되는 값은 any이며 나머지인경우는 never
-
 
 // 제네릭 Generic 이란?
 // 타입을 마치 함수의 파라미털처럼 사용하는 것
---
+++ basic-types/void.ts
@@ -0,0 +1,9 @@
+// 함수 선언
+function returnVoid(message: string){
+  console.log(message);
+
+  return;
+}
+
+// 함수 사용
+returnVoid("리턴이 없다.") // r의 타입은 void 
